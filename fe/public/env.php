d21dc8f2c6f202decbbe502681308d8216df0895
vishal <vishal.kumar1145@gmail.com>
2024-06-08 19:29:56 +0530

    new fe change env

--- fe/public/env.php
+++ fe/public/env.php
@@ -1,7 +1,7 @@
 <?php 
 
 // $baseUrl = "http://localhost:5000/";
-$baseUrl1 = "https://uat.practiceapi.doubtbuddy.com/";
+// $baseUrl1 = "https://uat.practiceapi.doubtbuddy.com/";
 $baseUrl = "https://prodapi.doubtbuddy.com/";
 
 ?>
